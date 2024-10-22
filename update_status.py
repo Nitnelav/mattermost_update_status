2143e849142d673df9f97b4fe3c5c40045670eb3
Valentin LE BESCOND <valentin.lebescond@univ-eiffel.fr>
2024-10-22 14:24:33 +0200

    Update schedule.csv and update_status.py

--- update_status.py
+++ update_status.py
@@ -20,18 +20,19 @@ mm.login(email, password)
 
 user = mm.get_user()
 id = user["id"]
-
-now = datetime.datetime.now()
-current_week_day = now.strftime('%A')
-current_day = now.strftime('%Y-%m-%d')
-
-schedule_days = [day for day in schedule if day['day'] == current_week_day]
-
 res = None
-if len(schedule_days) > 0:
-    emoji = schedule_days[0]['emoji']
-    text = schedule_days[0]['text']
-    status = '{"emoji":"%s","text":"%s","duration":"today","expires_at":"%sT20:00:00.000Z"}' % (emoji, text, current_day)
-    res = mm.patch_user(id, props={"props" : {'customStatus': status}})
 
-assert res is not None, "Error updating status"
+if (user["props"]["customStatus"] == ''):
+    now = datetime.datetime.now()
+    current_week_day = now.strftime('%A')
+    current_day = now.strftime('%Y-%m-%d')
+
+    schedule_days = [day for day in schedule if day['day'] == current_week_day]
+
+    if len(schedule_days) > 0:
+        emoji = schedule_days[0]['emoji']
+        text = schedule_days[0]['text']
+        status = '{"emoji":"%s","text":"%s","duration":"today","expires_at":"%sT20:00:00.000Z"}' % (emoji, text, current_day)
+        res = mm.patch_user(id, props={"props" : {'customStatus': status}})
+
+print(res)
