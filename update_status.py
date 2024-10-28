0eb5e08e415954b60e38d48ba9ceee62c5d1db07
Valentin LE BESCOND <valentin.lebescond@univ-eiffel.fr>
2024-10-28 09:54:17 +0100

    Update update_status.py

--- update_status.py
+++ update_status.py
@@ -1,5 +1,6 @@
 import mattermost
-import datetime
+from datetime import datetime as dt, UTC
+import json
 import os
 import csv
 
@@ -8,31 +9,63 @@ server = os.environ.get('MATTERMOST_SERVER')
 email = os.environ.get('MATTERMOST_EMAIL')
 password = os.environ.get('MATTERMOST_PASSWORD')
 
+if server is None:
+    print("MATTERMOST_SERVER environment variable is not set. Exiting.")
+    exit()
+
+if email is None:
+    print("MATTERMOST_EMAIL environment variable is not set. Exiting.")
+    exit()
+
+if password is None:
+    print("MATTERMOST_PASSWORD environment variable is not set. Exiting.")
+    exit()
+
 # Load csv data
 schedule = []
-with open('schedule.csv', mode='r') as file:
-    csv_reader = csv.DictReader(file, delimiter=';')
-    schedule = [row for row in csv_reader]
+try:
+    with open('schedule.csv', mode='r') as file:
+        csv_reader = csv.DictReader(file, delimiter=';')
+        schedule = [row for row in csv_reader]
+except FileNotFoundError:
+    print("File schedule.csv not found. Exiting.")
+    exit()
+except Exception as e:
+    print("Error reading schedule.csv: %s" % e)
+    exit()
 
 # login framateam
 mm = mattermost.MMApi("https://%s/api" % server)
-mm.login(email, password)
+login_res = mm.login(email, password)
+
+if login_res is None:
+    print("Login failed. Exiting.")
+    exit()
 
 user = mm.get_user()
 id = user["id"]
 res = None
 
-if (user["props"]["customStatus"] == ''):
-    now = datetime.datetime.now()
-    current_week_day = now.strftime('%A')
-    current_day = now.strftime('%Y-%m-%d')
+now = dt.now(UTC)
 
-    schedule_days = [day for day in schedule if day['day'] == current_week_day]
+if (user["props"]["customStatus"] != ''):
+    expireDate = dt.fromisoformat(json.loads(user["props"]["customStatus"])["expires_at"])
+    if now < expireDate:
+        print("Custom status already set. Do nothing.")
+        exit()
 
-    if len(schedule_days) > 0:
-        emoji = schedule_days[0]['emoji']
-        text = schedule_days[0]['text']
-        status = '{"emoji":"%s","text":"%s","duration":"today","expires_at":"%sT20:00:00.000Z"}' % (emoji, text, current_day)
-        res = mm.patch_user(id, props={"props" : {'customStatus': status}})
+current_week_day = now.strftime('%A')
+current_day = now.strftime('%Y-%m-%d')
 
-print(res)
+schedule_days = [day for day in schedule if day['day'] == current_week_day]
+
+if len(schedule_days) < 0:
+    print("No schedule for today. Do nothing.")
+    exit()
+
+emoji = schedule_days[0]['emoji']
+text = schedule_days[0]['text']
+status = '{"emoji":"%s","text":"%s","duration":"today","expires_at":"%sT20:00:00.000Z"}' % (emoji, text, current_day)
+res = mm.patch_user(id, props={"props" : {'customStatus': status}})
+
+print("Status Updated: %s" % status)
